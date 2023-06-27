aad760476be4dd2889a3d8788a53e2eeef69c063
christinacephus@pursuit.org <christinacephus@pursuit.org>
2023-06-26 21:46:15 -0400

    starting Node class

--- solution.js
+++ solution.js
@@ -1,6 +1,15 @@
 const { nums, words } = require("./data/data.js");
 
+class Node {
+  constructor(data, next) {
+    this.data = data;
+    this.next = null;
+}
+}
+
+
 module.exports = {
   Node,
   LinkedList,
 };
+
