b32b709626bc983cfa22276512f3f903018086e1
christinacephus@pursuit.org <christinacephus@pursuit.org>
2023-06-27 19:51:26 -0400

    added delete + comments

--- solution.js
+++ solution.js
@@ -32,8 +32,26 @@ class LinkedList {
     }
     return count;
    }
-   
-  // delete()
+
+  delete(data) {
+    let node = this.head;
+    let counter = 0;
+    while (node.data !== data && node.next) { // this while compares the data at index to the data we are looking to delete 
+      counter++; //keeps incrementing
+      node = node.next; // updates the node to the next node index
+    }
+    let foundNode = node; //intros a new instance of the found value for deletion
+    node = this.head; //rest to beginning of the list
+    for (let i = 1; i < counter; i++) 
+    // this loop is to loop through the entire list from before the foundNode value.
+    {
+      node = node.next;
+      //this loop keeps going until right before the foundNode value all because of counter keeping track of the index of foundNode.
+      
+    }
+    node.next = foundNode.next;
+    //takes the current node and drops the foundNode and connects to the next value
+  }
   // getFirst()
   // getLast()
   // search()
