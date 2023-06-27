8907d8fac689ab9b0e00777d277b25f4426f2af2
pjungjs <pjungjs@gmail.com>
2023-06-27 19:21:14 -0400

    add insert methods. one to add in the beginning and the other to add at the end

--- solution.js
+++ solution.js
@@ -11,6 +11,32 @@ class LinkedList {
   constructor(head = null) {
     this.head = head;
   }
+
+  insert(value) {
+    //insert the value at the beginning on the list.
+    let newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+      // [newNode.next, this.head] = [this.head, newNode];
+    }
+  }
+
+  insertAtLast(value) {
+    //insert the value at the last on the list.
+    let newNode = new Node(value);
+    let currentNode = this.head;
+    if (currentNode === null) {
+      this.head = newNode;
+    } else {
+      while(currentNode.next) {
+        currentNode = currentNode.next;
+      }
+      currentNode.next = newNode;
+    }
+  }
 }
 
 module.exports = {
