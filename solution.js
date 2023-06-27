4c64e89392d9fb3a921fa144051e2d128f72bb99
christinacephus@pursuit.org <christinacephus@pursuit.org>
2023-06-26 22:06:46 -0400

    added lists arrays & clear method

--- solution.js
+++ solution.js
@@ -20,12 +20,28 @@ class LinkedList {
   search()
   getKthToLast()
   isEmpty()
-  
-  clear() {
 
+  clear() {
+    this.head = null;
   }
   containsDuplicates()
 }
+
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 0];
+
+const panagram = [
+  "The",
+  "quick",
+  "brown",
+  "fox",
+  "jumps",
+  "over",
+  "the",
+  "lazy",
+  "dog",
+];
+
+console.log(list
 
 
 module.exports = {
