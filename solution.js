e23a04555032c79649aeeacc63b66af3db2e29c0
pjungjs <pjungjs@gmail.com>
2023-06-26 22:20:29 -0400

    add Node and LinkedList class. add its constructor.

--- solution.js
+++ solution.js
@@ -1,5 +1,18 @@
 const { nums, words } = require("./data/data.js");
 
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(head = null) {
+    this.head = head;
+  }
+}
+
 module.exports = {
   Node,
   LinkedList,
