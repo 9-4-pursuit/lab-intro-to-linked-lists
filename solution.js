f6926c41fb9e4be5faf9448779918f8dbe161ebb
laura-williams-1 <laurawilliams@pursuit.org>
2023-07-10 21:46:24 -0400

    insert method created

--- solution.js
+++ solution.js
@@ -11,8 +11,25 @@ class Node {
 class LinkedList {
   constructor(head = null) {
     this.head = head;
+    this.size = 1;
+  }
+
+  insert(data) {
+    if (!this.head) {
+      this.head = new Node(data);
+    }
+    let current = this.head;
+    let newNode = new Node(data);
+    this.head = newNode;
+    this.head.next = current;
+    this.size++;
   }
 }
+// let node1 = new Node(20);
+// let ll = new LinkedList(node1);
+// ll.insert(10);
+// ll.insert(5);
+// console.log(node1, ll);
 
 module.exports = {
   Node,
