1a9e338add86ea7010cbee2990a9a149a492ecd5
jkpasaoa <jacqueline.pasaoa@gmail.com>
2023-07-10 21:36:32 -0400

    add new linkedlist and insert method

--- solution.js
+++ solution.js
@@ -1,11 +1,26 @@
 const { nums, words } = require("./data/data.js");
 
 class Node {
-
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
 }
 
 class LinkedList {
+  constructor(head = null) {
+    this.head = head;
+  }
 
+  insert(data) {
+    let node = new Node(data);
+    if (!this.head) {
+      this.head = node;
+    } else {
+      node.next = this.head;
+      this.head = node;
+    }
+  }
 }
 
 module.exports = {
