f0c49d5b5af2447035bc37a2637fcade0f675afe
laura-williams-1 <laurawilliams@pursuit.org>
2023-07-10 21:29:27 -0400

    node class created w. constuctor

--- solution.js
+++ solution.js
@@ -1,5 +1,15 @@
 const { nums, words } = require("./data/data.js");
 
+class Node {
+  // every node has a data + a pointer
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class LinkedList {}
+
 module.exports = {
   Node,
   LinkedList,
