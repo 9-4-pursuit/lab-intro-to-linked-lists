4b4ca989c7ec6a67a4e8143fab0365373aa78c56
SamuelBAlba <brayanbalba24@gmail.com>
2023-07-17 19:06:24 -0400

    working through

--- solution.js
+++ solution.js
@@ -1,5 +1,84 @@
 const { nums, words } = require("./data/data.js");
 
+
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(head = null) {
+    this.head = head;
+  }
+
+  insert(data) {
+    let node = new Node(data);
+
+    if (!this.head) {
+      this.head = node
+    }else {
+      node.next = this.head
+      this.head = node
+    }
+  };
+
+  size() {
+    let count = 0;
+    let currentNode = this.head
+  
+    // while (currentNode != null) also works
+    while (currentNode) {
+      count++
+      currentNode = currentNode.next
+    }
+    return count;
+  };
+
+  delete(value) {
+    let currentNode = this.head;
+    if (currentNode.data === value){
+      this.head = currentNode.next
+      return this.head;
+    };
+    while (currentNode.next) {
+      if (currentNode.next.data === value) {
+        currentNode.next = currentNode.next.next;
+        return this.head;
+      };
+      currentNode = currentNode.next;
+    }
+  };
+
+  getFirst() {
+    return this.head;
+  };
+
+  getLast() {
+    let currentNode = this.head;
+
+    while (currentNode.next != null) {
+      currentNode = currentNode.next
+    };
+    return currentNode;
+  }
+
+  search(value) {
+    let currentNode = this.head
+  }
+
+
+
+
+}
+
+
+
+
+
+
+
 module.exports = {
   Node,
   LinkedList,
