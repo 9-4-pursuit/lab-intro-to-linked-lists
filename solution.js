0c2abee2f8fcdb5cf3f0a272dfbbaa99059e9701
SamuelBAlba <brayanbalba24@gmail.com>
2023-07-17 20:15:20 -0400

    all test

--- solution.js
+++ solution.js
@@ -65,11 +65,72 @@ class LinkedList {
   }
 
   search(value) {
-    let currentNode = this.head
+    let currentNode = this.head;
+    while (currentNode != null) {
+      if (currentNode.data === value) {
+        return currentNode;
+      }
+      currentNode = currentNode.next
+    }
+    return null
   }
 
+  getKth(k) {
+    let currentNode = this.head;
+    let count = 1;
+    
+    while (currentNode) {
+      if (count === k) {
+        return currentNode
+      }
+      currentNode = currentNode.next
+      count++
+    }
+  }
 
+  getKthToLast(index) {
+    let currentNode = this.head;
+    let count = 1;
+    let newIndex = this.size() - index;
 
+    while (currentNode != null) {
+      if (count === newIndex) {
+        return currentNode;
+        // currentNode = currentNode.next;
+        // count++;
+      }
+      currentNode = currentNode.next;
+      count++;
+    }
+  }
+
+  isEmpty() {
+    return !this.head
+  }
+
+  clear() {
+    this.head = null;
+  }
+
+  toArray() {
+    let currentNode = this.head;
+    let arr = []
+
+    while (currentNode) {
+      arr.push(currentNode.data);
+      currentNode = currentNode.next;
+    }
+    return arr
+  }
+
+  containsDuplicates() {
+    const arr = this.toArray();
+    const set = new Set(arr)
+
+    return arr.length !== set.size;
+    
+
+  }
 
 }
 
