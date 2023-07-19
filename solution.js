8ea5bd9dfe8e5ef5aebde3851f481b82b86414fa
jRodriguezIV <j.a.rodrigueziv@gmail.com>
2023-07-19 02:04:11 -0400

    created Node and LinkedList classes, created insert and size methods

--- solution.js
+++ solution.js
@@ -1,5 +1,37 @@
 const { nums, words } = require("./data/data.js");
 
+  class Node {
+    constructor(data) {
+      this.data = data
+      this.next = null
+    }
+  }
+
+  class LinkedList {
+    constructor( head= null) {
+      this.head = head
+    }
+
+    insert(data) {
+      let newNode = new Node(data)
+      if(!this.head) {
+        this.head = newNode
+      } else {
+        newNode.next = this.head
+        this.head = newNode
+      }
+    }
+
+    size() {
+      let count = 0
+      let node = this.head
+      while(node){
+        count++
+        node = node.next
+      }
+    }
+  }
+
 module.exports = {
   Node,
   LinkedList,
